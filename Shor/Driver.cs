577ec3d21199c964d8b7d75c3054c96e3c0a8316
WantonWind <875357936@qq.com>
2018-08-07 21:41:16 +0800

    comments added.

--- Shor/Driver.cs
+++ Shor/Driver.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿// Created by Jiuqin Zhou and Yaoyao Ding.
+// Licensed under the MIT License.
+
+using System;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
+using System.IO;
 
 namespace Shor
 {
     using myShor;
     using System.Collections;
-    using System.Numerics;
 
     class Driver
     {
@@ -22,51 +25,106 @@ namespace Shor
             a = tmp;
         }
 
-        static (int, int) GetFraction(ArrayList list, int ind)
-        {
-            int ns = 1;
-            int r = (int)list[--ind];
-            while (ind > 0)
-            {
-                ns = ns + (int)list[--ind] * r;
-                Swap(ref ns, ref r);
+        static void w2qasm(string msg){
+            using (FileStream fs = new FileStream("test.qasm", FileMode.Create, FileAccess.Write)){
+                using (StreamWriter sw = new StreamWriter(fs)){
+                    sw.WriteLine(msg);
+                }
+           }
+        }      
+
+       static void CircuitMaking(int a,int n){
+            string str="";
+            int k=0;
+            int n1=n;
+            while (n1>0){
+                ++k;
+                n1/=2;
             }
-            Swap(ref ns, ref r);
-            return (ns, r);
+            int k2=2*k+1;
+            int temp=1;
+            string tempstr="";
+            for (int i=0;i<k2;++i){
+                if (temp==1) tempstr="\'U\'\n";
+                else tempstr="\'U^{"+temp.ToString()+"}\'\n";
+                temp*=2;
+                str+="\tdefbox CU"+i.ToString()+","+(k+1).ToString()+",1,"+tempstr;
+            }
+            for (int i=2;i<=k2;++i){
+                str+="\tdef CR"+i.ToString()+",1,"+"\'R"+i.ToString()+"\'\n";
+            }
+            for (int i=0;i<k2;++i){
+                str+="\tqubit i"+i.ToString()+",0\n";
+            }
+            for (int i=0;i<k;++i){
+                str+="\tqubit j"+i.ToString()+",0\n";
+            }
+            for (int i=0;i<k2;++i){
+                str+="\th i"+i.ToString()+"\n";
+            }
+            str+="\tX j0\n";
+            for (int i=0;i<k2;++i){
+                str+="\tCU"+i.ToString()+" i"+(k2-i-1).ToString();
+                for (int j=0;j<k;++j){
+                    str+=",j"+j.ToString();
+                }         
+                str+="\n";     
+                for (int j=k2-i;j<k2;++j){
+                    str+="\tnop i"+j.ToString()+"\n";
+                }
+            }
+            for (int i=0;i<k2;++i){
+                for (int j=0;j<i;++j){
+                    int delta=i-j+1;
+                    str+="\tCR"+delta.ToString()+" i"+j.ToString()+",i"+i.ToString()+"\n";
+                }
+                str+="\th i"+i.ToString()+"\n";
+                for (int j=0;j<i;++j){
+                    for (int l=0;l<i-j;++l)
+                        str+="\tnop i"+j.ToString()+"\n";
+                }
+            }
+            for (int i=0;i<k2;++i){
+                str+="\tmeasure i"+i.ToString()+"\n";
+            }
+            w2qasm(str);
         }
 
-        static int ContinuedFraction(double s, int a, int n)
+        static int ContinuedFraction(double s)
         {
-            const double eps = 1e-9;
+            const double eps = 0.1;
             double ss = s;
-            ArrayList list = new ArrayList();
+            Stack st = new Stack();
             while (true)
             {
-                list.Add((int)s);
-                s = s - (int)(s);
+                st.Push(((int)(s + eps)));
+                s = s - (int)(s + eps);
                 if (s < eps)
                 {
                     break;
                 }
                 s = 1 / s;
             }
-            for (int i = 1; i <= list.Count; ++i)
+            int ns = 1;
+            int r = (int)st.Pop();
+            while (st.Count != 0)
             {
-                (int ns, int r) = GetFraction(list, i);
-                if(Mpow(a, r, n) == 1)
-                {
-                    Console.WriteLine($"\t= {ns}/{r}");
-                    return r;
-                }
+                ns = ns + (int)st.Pop() * r;
+                Swap(ref ns, ref r);
             }
-            return -1; // not found
-        }
+
+            Swap(ref ns, ref r);
+            Console.WriteLine($"\t= {ns}/{r}");
+            return r;
+        } 
 
         static int OrderFinding(int a, int n)
         {
             double s;
+            CircuitMaking(a,n);
             using (var sim = new QuantumSimulator())
             {
+                // quantum order finding
                 long f = 0;
                 long t = 0;
                 while (f == 0)
@@ -80,8 +138,9 @@ namespace Shor
                 s = ((double)f) / (1L << (int)t);
                 Console.WriteLine($"{f}/(2^{t})={s}");
             }
-            return ContinuedFraction(s, a, n);
+            return ContinuedFraction(s);
 
+            // classic order finding
             //int r = 1;
             //int s = a % n;
             //while (s != 1)
@@ -92,6 +151,7 @@ namespace Shor
             //return r;
         }
 
+        // fast mod power : Exp(a, b) % n
         static int Mpow(long a, int b, int n)
         {
             long s;
@@ -105,6 +165,7 @@ namespace Shor
         static void output(int a, int b)
         {
             Console.WriteLine($"{a * b} = {a} * {b}");
+            Console.WriteLine($"Press any key to continue ...");
             Console.ReadKey();
             Environment.Exit(0);
         }
@@ -113,6 +174,7 @@ namespace Shor
         {
             Console.WriteLine(s + " " + $"{a}");
         }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Please input the integer:");
@@ -122,22 +184,22 @@ namespace Shor
             while (true)
             {
                 int a = randomObject.Next(2, n-1);
-                printInt("a = ", a);
+                printInt("a =", a);
                 int d = Gcd(a, n);
                 if (d != 1)
                 {
                     output(d, n / d);
                 }
                 int r = OrderFinding(a, n);
-                printInt("r = ", r);        
-                if (r == -1 || r % 2 == 1)
+                printInt("r =", r);
+                if (r % 2 == 1)
                     continue;
-                printInt("a^{r/2} = ", Mpow(a, r / 2, n));
+                printInt("a^{r/2} =", Mpow(a, r / 2, n));
                 if (Mpow(a, r / 2, n) == n - 1)
                     continue;
 
                 int f = Math.Max(Gcd(Mpow(a, r / 2, n) + 1, n), Gcd(Mpow(a, r / 2, n) - 1, n));
-                printInt("f = ", f);
+                printInt("f =", f);
                 output(f, n / f);
             }
         }
--- Shor/Operation.qs
+++ Shor/Operation.qs
@@ -1,8 +1,37 @@
-﻿namespace myShor {
+﻿// Created by Jiuqin Zhou and Yaoyao Ding.
+// Licensed under the MIT License.
+
+namespace myShor {
     open Microsoft.Quantum.Primitive;
 	open Microsoft.Quantum.Canon;
 	open Microsoft.Quantum.Extensions.Math;
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Introduction ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    ///
+    /// This sample contains Q# code implementing Shor's quantum algorithm for
+    /// factoring integers. The underlying modular arithmetic is implemented 
+    /// in phase encoding, based on a paper by Stephane Beauregard who gave a
+    /// quantum circuit for factoring n-bit numbers that needs 2n+3 qubits and 
+    /// O(n³log(n)) elementary quantum gates.
+    ///
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/// # Summary 
+    /// U|x> -> |ax mod N >
+    ///
+    /// # Input 
+    /// ## a
+    /// Constant by which multiplier is being multiplied. Must be co-prime to modulus.
+    /// ## N
+	/// The multiplication operation is performed modulo modulus.
+	/// ## x
+	/// The number being multiplied by a constant. This is an array of qubits 
+	/// representing integer in little-endian bit order.
+    ///
+    /// # Output 
+    /// None.
 	operation OrderFindingOracle(a : Int, N : Int, x : Qubit[]) : () 
 	{
 		body
@@ -12,6 +41,15 @@
 		controlled auto
 	}
 
+	/// # Summary
+	/// Exponentiation function with base 2 and exponent x.
+	///
+	/// # Input
+	/// ## x
+	/// An Integer representing exponent.
+	/// 
+	/// # Output
+	/// Exp(2, x)
 	function Exp2(x : Int) : (Double)
 	{
 		mutable ans = 1.0;
@@ -22,6 +60,16 @@
 		return ans;
 	}
 
+
+	/// # Summary
+	/// operating quantum fourier transform on x.
+	///
+	/// # Input
+	/// ## x
+	/// The array of qubits to be operated on with QFT.
+	/// 
+	/// # Output
+	/// None.
 	operation QuantumFourierTransform(x : Qubit[]) : () 
 	{
 		body 
@@ -44,6 +92,21 @@
 		adjoint auto
 	}
 
+	/// # Summary
+	/// In the order finding with Exp(x, r)=1 mod N, find approximate s/r 
+	/// with the numerator ans and the dominator Exp(2, t)
+	///
+	/// # Input
+	/// ## x
+	/// Order finding with Exp(x, r)=1 mod N.
+	/// ## N
+	/// Order finding with Exp(x, r)=1 mod N.
+	///
+	/// # Output
+	/// ## ans
+	/// ans is the numerator of approximate s/r
+	/// ## t
+	/// Exp(2, t) is the dominator of approximate s/r
 	operation OrderFindingPhase(x : Int, N : Int) : (Int, Int) 
 	{
 		body
